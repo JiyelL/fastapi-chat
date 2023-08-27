673455619dff9d5437bcd11c5b1cd6b7c9c0c4b5
JiyelL <gsarmiento0798@gmail.com>
2023-08-27 15:05:58 +0800

    Update on CORS

--- main.py
+++ main.py
@@ -2,8 +2,24 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from hugchat import hugchat
 from hugchat.login import Login
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Message(BaseModel):
     message: str
